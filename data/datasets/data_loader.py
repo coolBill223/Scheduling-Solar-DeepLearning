374a919798f9bfcb2f163a5d802fef6d8907ef24
coolBill223 <bzhou6198@gmail.com>
2025-03-17 12:41:18 -0400

    check

--- data/datasets/data_loader.py
+++ data/datasets/data_loader.py
@@ -28,6 +28,8 @@ def load_data():
         return None, None, None, None 
 
     df = pd.read_excel(file_path, engine="openpyxl")
+    df_raw = pd.read_excel(file_path, engine="openpyxl", dtype=str)  # 以字符串格式读取，防止数据丢失
+    print(df_raw[target].head(10))
     df.columns = df.columns.str.strip().str.replace("\n", " ")
 
     if df.empty:
