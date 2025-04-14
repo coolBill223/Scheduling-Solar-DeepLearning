bd122ad012162539da6521d23cacb76ee25c6453
coolBill223 <bzhou6198@gmail.com>
2025-04-14 12:11:39 -0400

    update model

--- models/solar_lr_model.py
+++ models/solar_lr_model.py
@@ -1,11 +1,21 @@
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
+import numpy as np
 
 class SklearnLRWrapper:
-    def __init__(self):
-        self.model = LinearRegression()
+     def __init__(self, alpha=1.0):
+        self.model = Ridge(alpha=alpha)
+        self.feature_names = None
+        self.coef_ = None
 
-    def fit(self, X, y):
+     def fit(self, X, y, feature_names=None):
+        self.feature_names = feature_names
         self.model.fit(X.numpy(), y.numpy().flatten())
+        self.coef_ = self.model.coef_
 
-    def predict(self, X):
+     def predict(self, X):
         return self.model.predict(X.numpy()).reshape(-1, 1)
+
+     def get_coefficients(self):
+        if self.coef_ is None:
+            return None
+        return sorted(zip(self.feature_names, self.coef_), key=lambda x: abs(x[1]), reverse=True)
--- models/solar_tree_model.py
+++ models/solar_tree_model.py
@@ -1,12 +1,26 @@
-from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+import numpy as np
 
 class SklearnTreeWrapper:
-    def __init__(self, **kwargs):
-        self.model = DecisionTreeRegressor(**kwargs)
+    def __init__(self, n_estimators=100, max_depth=6, random_state=42):
+        self.model = RandomForestRegressor(
+            n_estimators=n_estimators,
+            max_depth=max_depth,
+            random_state=random_state,
+            n_jobs=-1
+        )
+        self.feature_names = None
+        self.feature_importances_ = None
 
-    def fit(self, X, y):
+    def fit(self, X, y, feature_names=None):
+        self.feature_names = feature_names
         self.model.fit(X.numpy(), y.numpy().flatten())
+        self.feature_importances_ = self.model.feature_importances_
 
     def predict(self, X):
-        import numpy as np
         return self.model.predict(X.numpy()).reshape(-1, 1)
+
+    def get_feature_importance(self):
+        if self.feature_importances_ is None:
+            return None
+        return sorted(zip(self.feature_names, self.feature_importances_), key=lambda x: -x[1])
