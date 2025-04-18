1208e05aaa8938fd2f623c7b7d58e7b2ca47db40
coolBill223 <bzhou6198@gmail.com>
2025-04-17 21:44:31 -0400

    update gitignore and app

--- app_entry.py
+++ app_entry.py
@@ -1,10 +1,11 @@
 import os
-import subprocess
 import threading
 import webbrowser
-from main import app 
+from main import app
 
+def open_browser():
+    webbrowser.open("http://127.0.0.1:5000")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    threading.Timer(1.5, open_browser).start()
+    app.run(debug=False)
