34b0f79fd41084c7a9c9cc607bf8eca016a013cf
coolBill223 <bzhou6198@gmail.com>
2025-04-17 20:50:06 -0400

    update appentry

--- app_entry.py
+++ app_entry.py
@@ -4,9 +4,7 @@ import threading
 import webbrowser
 from main import app 
 
-def open_browser():
-    webbrowser.open("http://127.0.0.1:5000")
 
 if __name__ == "__main__":
-    threading.Timer(1.5, open_browser).start()
-    app.run(debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
